d41f97f0891459f6844b32138ae9ac99756c4412
lynn <tharlynnhtet.ong@gmail.com>
2026-08-13 17:53:20 -0700

    Inital Setup

---
+++ demo/src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+  import Parallax from "../../../src/Parallax.svelte";
+</script>
+
+<Parallax></Parallax>
---
+++ src/Parallax.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import { writable } from "svelte/store";
+
+  let y = writable(0);
+  let innerHeight = writable(0);
+</script>
+
+<svelte:window bind:scrollY={$y} bind:innerHeight={$innerHeight} />
+
+<div class="parallax-container">
+  Parallax container!
+  <slot />
+</div>
+
+<style>
+  .parallax-container {
+    position: relative;
+    overflow: hidden;
+    box-sizing: border-box;
+  }
+</style>
